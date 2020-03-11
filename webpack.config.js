63dceb8cda582707cb2b434fc3a3cf1caeaaf3b8
Kieran Scannell <kierscan@gmail.com>
2020-03-11 07:32:42 -0500

    Refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: './main.js',
+    filename: './bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   devtool: 'source-map'
